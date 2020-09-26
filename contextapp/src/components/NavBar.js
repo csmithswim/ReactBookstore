b1913149c144667dfabc9c37aee6f3efe2ed5f83
Cullen Smith <cullensmi@gmail.com>
2020-09-26 15:56:20 -0400

    added consumer context

--- contextapp/src/components/NavBar.js
+++ contextapp/src/components/NavBar.js
@@ -1,18 +1,25 @@
 import React , { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
  class navBar extends Component {
     render() {
         return (
-            <nav>
+        <ThemeContext.Consumer>{(context) => {
+            const { isLightTheme, light, dark } = context;
+            const theme = isLightTheme ? light: dark;
+            return(
+                <nav style={{background: theme.ui, color: theme.syntax }}>
                 <h1> Context App</h1>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact me</li>
                 </ul>
-            </nav>                
-        )
+            </nav>               
+            )
+            }}</ThemeContext.Consumer>            
+        );
     }
-};
+}
 
 export default navBar;
