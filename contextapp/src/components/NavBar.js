626e56b13947f54f4f89279a0838f19ec7c8d908
Cullen Smith <cullensmi@gmail.com>
2020-09-26 12:11:14 -0400

    first commit

---
+++ contextapp/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React , { Component } from 'react';
+
+ class navBar extends Component {
+    render() {
+        return (
+            <nav>
+                <h1> Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact me</li>
+                </ul>
+            </nav>                
+        )
+    }
+};
+
+export default navBar;
