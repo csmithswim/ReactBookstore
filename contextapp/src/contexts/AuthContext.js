5e6153230ba4ae06d5e6c50104410b7f2bbdee0c
Cullen Smith <cullensmi@gmail.com>
2020-09-27 11:11:26 -0400

    first commit

---
+++ contextapp/src/contexts/AuthContext.js
@@ -0,0 +1,16 @@
+import React, { Component, createContext } from 'react';
+
+import React, { Component } from 'react'
+
+export default class AuthContextProvider extends Component {
+    state = {
+        isAuthenticated: false
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
