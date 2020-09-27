ab19c312bf1a3f2b680a24871e187e731fa9cc84
Cullen Smith <cullensmi@gmail.com>
2020-09-27 14:30:50 -0400

    added usestate hook and uuid functionality

--- react-context-hooks/hooksapp/src/components/SongList.js
+++ react-context-hooks/hooksapp/src/components/SongList.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v1 as uuid } from 'uuid';
 
 export default function SongList() {
+    const [songs, setSongs] = useState([
+        { title: 'let me ride', id: 1 },
+        { title: 'carpet burn', id: 2 },
+        { title: 'love', id: 3 }
+    ]);
+    const addSong = () => {
+        setSongs([...songs, {title: 'new song', id: uuid()}])
+    }
     return (
-        <div>
+        <div className="song-list">
+            <ul>
+                {songs.map(song => {
+                    return ( <li key={song.id}>{song.title}</li>)
+                })}
+            </ul>
+            <button onClick = {addSong}>Add a song</button>
             
         </div>
     )
