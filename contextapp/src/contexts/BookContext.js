fdae44fd432bcd01f5198147ea4691de94a6e899
Cullen Smith <cullensmi@gmail.com>
2020-10-05 12:36:28 -0400

    final commit

--- contextapp/src/contexts/BookContext.js
+++ contextapp/src/contexts/BookContext.js
@@ -1,2 +1,19 @@
 import React, { createContext, useState } from 'react';
 
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([
+        {title: 'beloved', id: 1},
+        {title: 'dracula', id: 2},
+        {title: 'frankenstein', id: 3}   
+    ]);
+    return (
+        <BookContext.Provider value={{books}}>
+            {props.children}
+        </BookContext.Provider>
+    )
+}
+
+
+export default BookContextProvider;
