c7b105972bd77f0f11c0d5b4dca3ef5960235100
Cullen Smith <cullensmi@gmail.com>
2020-10-05 12:37:08 -0400

    final commit

--- contextapp/src/App.js
+++ contextapp/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import BookList from './components/BookList'
 import NavBar from './components/NavBar'
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
+import BookContextProvider from './contexts/BookContext';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-    <NavBar />
+    <ThemeContextProvider>
+      <AuthContextProvider>
+        <NavBar />
+        <BookContextProvider>
+          <BookList />
+      </BookContextProvider>
+      <ThemeToggle />
+    </AuthContextProvider>
+  </ThemeContextProvider>
+    
     </div>
   );
 }
